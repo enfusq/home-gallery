2f2402d3cc495df8648336147b4173065a22fc18
Aivars Trumars <aivars.trumars@gmail.com>
2026-04-17 00:56:23 +0300

    Correctly handle system bars

--- app/src/main/java/com/example/homegallery/ui/HomeGalleryApp.kt
+++ app/src/main/java/com/example/homegallery/ui/HomeGalleryApp.kt
@@ -3,25 +3,19 @@
 package com.example.homegallery.ui
 
 import android.net.Uri
-import android.widget.ImageView
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.homegallery.R
 import com.example.homegallery.ui.screens.HomeScreen
@@ -39,12 +33,11 @@ fun HomeGalleryApp() {
                 }
             )
         },
-        modifier = Modifier
-            .statusBarsPadding()
-            .navigationBarsPadding()
-    ) {
+    ) { innerPadding ->
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .consumeWindowInsets(innerPadding),
         ) {
             HomeScreen(
                 imageUiState = imageViewModel.imageUiState,
@@ -54,7 +47,7 @@ fun HomeGalleryApp() {
                 onDeleteImage = { imageViewModel.deleteImage(it) },
                 onDownloadImage = {imageViewModel.downloadImage(it)},
                 downloadSuccess = imageViewModel.downloadSuccess,
-                contentPadding = it
+                contentPadding = innerPadding
             )
         }
     }
